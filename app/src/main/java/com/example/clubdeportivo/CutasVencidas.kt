014fd241307ad68f5ff2cbb8421615f8feb5a380
Ivan <ivanfgonzalezruiz@gmail.com>
2024-10-13 13:12:18 -0300

    ventana de vencimiento cuota

---
+++ app/src/main/java/com/example/clubdeportivo/CutasVencidas.kt
@@ -0,0 +1,16 @@
+package com.example.clubdeportivo
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class CutasVencidas : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_cutas_vencidas)
+
+    }
+}
