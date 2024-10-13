75421ea27665d6b424946ef616d7bfa379b9e390
Sando <santiagotomas16@gmail.com>
2024-10-13 16:15:23 -0300

    Algunos ajustes a las pantallas

---
+++ app/src/main/java/com/example/clubdeportivo/CuotasVencidas.kt
@@ -0,0 +1,13 @@
+package com.example.clubdeportivo
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class CuotasVencidas : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_cuotas_vencidas)
+
+    }
+}
